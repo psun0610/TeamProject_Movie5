90ac06e5bd84c77a367315444fedf629b7939387
suyoung049 <suyoung049@gmail.com>
2022-10-14 11:32:11 +0900

    update signup

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import get_user_model
+from .forms import Myform
 
 # Create your views here.
+def signup(request):
+    if request.method == "POST":
+        form = Myform(request.POST)
+        if form.is_valid():
+            form.save()
+            return
+    else:
+        form = Myform()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
